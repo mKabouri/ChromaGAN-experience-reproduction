eb8405c27648c741cc624bf21f02e9b25aca385b
Mohamed Yassine KABOURI <106626178+mKabouri@users.noreply.github.com>
2023-05-10 12:17:16 +0200

    Skimage instead of cv2

--- src/Data.py
+++ src/Data.py
@@ -1,8 +1,9 @@
-import numpy as np
-import cv2
 import os
 import config
 
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray, rgb2lab, lab2rgb
+import numpy as np
 
 def loadData(file):
     import pickle
@@ -16,9 +17,24 @@ def deprocess(imgs):
     imgs[imgs < 0] = 0
     return imgs.astype(np.uint8)
 
-def concatenateLabToRGB(gray, ab):
-    concat = np.concatenate((gray, ab), axis=2)
-    return cv2.cvtColor(concat, cv2.COLOR_Lab2RGB)
+def normalize_lab(lab):
+    L, a, b = lab[:,:,0], lab[:,:,1], lab[:,:,2]
+    L_norm = L / 100.0
+    a_norm = a / 127.0
+    b_norm = b / 127.0
+    image_lab_norm = np.stack([L_norm, a_norm, b_norm], axis=2)
+    return image_lab_norm
+
+def denormalize_lab(lab_norm):
+    L_norm, a_norm, b_norm = lab_norm[:,:,0], lab_norm[:,:,1], lab_norm[:,:,2]
+    L = L_norm * 100.0
+    a = a_norm * 127.0
+    b = b_norm * 127.0
+    image_lab = np.stack([L, a, b], axis=2)
+    return image_lab
+
+def backToRGBPipeline(gray, ab):
+    return lab2rgb(denormalize_lab(np.concatenate((gray, ab), axis=2)))
 
 
 class Data():
@@ -40,22 +56,18 @@ class Data():
         return self.labelNames[0][b'label_names'][self.dataset[batchIndex][b'labels'][rowIndex]].decode("utf-8")
     
     def toGray(self, img):
-        if self.imagesFormat == 'Lab':
-            return  img[:,:,0]
-        return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    
-    def colorImg(self, img):
-        img = img/255
-        img = img.astype("float32")
-        if self.imagesFormat == 'Lab':
-            return cv2.cvtColor(img, cv2.COLOR_RGB2Lab)
-        return img
+        return rgb2gray(img)
 
     def __getLearningData(self):
-        trainImages = np.array([self.colorImg(np.moveaxis(x.reshape(3, 32, 32), 0, -1)) for t in self.dataset[:len(self.dataset) - 1] for x in t[b'data']])
-        testImages = np.array([self.colorImg(np.moveaxis(x.reshape(3, 32, 32), 0, -1)) for x in self.dataset[len(self.dataset)-1][b'data']])
-        y_train = np.array([y[:, :, 1:] for y in trainImages])
-        y_test = np.array([y[:, :, 1:] for y in testImages])  
+        trainImages = np.array([np.moveaxis(x.reshape(3, 32, 32), 0, -1) for t in self.dataset[:len(self.dataset) - 1] for x in t[b'data']])
+        testImages = np.array([np.moveaxis(x.reshape(3, 32, 32), 0, -1) for x in self.dataset[len(self.dataset)-1][b'data']])
+        
+        if config.ImagesFormat == 'Lab':
+            y_train = np.array([normalize_lab(rgb2lab(y))[:, :, 1:] for y in trainImages])
+            y_test = np.array([normalize_lab(rgb2lab(y))[:, :, 1:] for y in testImages])  
+        else:
+            y_train = trainImages.copy()
+            y_test = testImages.copy()
 
         X_train = np.array([self.toGray(y).reshape(32, 32, 1) for y in trainImages])
         X_test = np.array([self.toGray(y).reshape(32, 32, 1) for y in testImages])
@@ -85,12 +97,26 @@ class Data():
     
         print("Data (", config.ImagesFormat, ") is successfully saved in npy format at: ", config.rootDir)
 
+    def showImage(self, img):
+        plt.imshow(img)
+        plt.show()
 
+
+    
 if __name__ == '__main__':
     
     data = Data(config.ImagesFormat)
     
+    print(data.X_train.shape, data.y_train.shape)
+
     #data.save()
     
+    L = data.X_train[0]
+    print(L)
+    ab = data.y_train[0]
+
+    data.showImage(backToRGBPipeline(L, ab))
+
     print("Ok")  
     
+
